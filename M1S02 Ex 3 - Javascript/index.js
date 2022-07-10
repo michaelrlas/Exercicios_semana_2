719384e2f7b6769bc40127ce2dbb76c128e8052e
michaelrlas <michaelsilvamkt@gmail.com>
2022-07-10 19:10:52 -0300

    exercicio5

--- M1S02 Ex 3 - Javascript/index.js
+++ M1S02 Ex 3 - Javascript/index.js
@@ -1,4 +1,5 @@
 var n1 = window.prompt('Digite o primeiro número');
 var n2 = window.prompt('Digite o segundo número');
 var total = parseFloat(n1) + parseFloat(n2);
-document.write('<h4> a soma de ', n1 + ' e ', n2, ' é = ', total, '<h4>');
+document.write('<h4> a soma de ', n1 + ' e ', n2, ' é = ', total, '</h4>');
+5;
---
+++ M1S02 Ex 5 - Impar ou par/index.js
@@ -0,0 +1,6 @@
+var n1 = parseFloat(window.prompt('Digite um número'));
+if (n1 % 2 == 0) {
+  document.write('<h3> o número ', n1 + ' é ', ' par ', '</h3>');
+} else {
+  document.write('<h3> o número ', n1 + ' é ', ' ímpar ', '</h3>');
+}
