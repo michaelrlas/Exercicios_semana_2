63869b9ea45bcc2a3080d440463f65e2636f9592
michaelrlas <michaelsilvamkt@gmail.com>
2022-07-10 18:14:43 -0300

    exercicio3

---
+++ M1S02 Ex 3 - Javascript/index.js
@@ -0,0 +1,5 @@
+var n1 = window.prompt('Digite o primeiro número');
+var n2 = window.prompt('Digite o segundo número');
+var total = parseFloat(n1) + parseFloat(n2);
+document.write('<h4> a soma de ', n1 + ' e ', n2, ' é = ', total, '<h4>');
+76;
